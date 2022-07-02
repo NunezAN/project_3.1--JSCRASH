67c60075b2edac1ec826f90eabcefebcc3a2ff31
NunezAN <alex101nunez@gmail.com>
2022-07-01 20:03:09 -0500

    equality variables

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 // let celsius =10;
 // let fahrenheit = (celsius * 1.8)+ 32;
 // console.log(fahrenheit);
+
+let bool = '1' !== 1;  //== compares value === compares values and type
+                        //!= compers not value !== compares not value and type
+console.log(bool);
