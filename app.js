80e5ce32eecc01e9b35bece5ed473f3e4a3f4d2c
NunezAN <alex101nunez@gmail.com>
2022-07-03 10:11:25 -0500

    loop example question

--- app.js
+++ app.js
@@ -85,6 +85,20 @@
 // }
 
 //for loop
-for ( let i=100;i>0;i--){
-    console.log(i);
+
+//example: write a for loop that loops thorugh 1-20 and if divisible by 3 print frontend, if divisible by 5 print simplified, if divisible by 3 and 5 pring frontend simplified, if not divisible by 3 or 5 print the number 
+for(i=1; i<=20;i++){
+    if(i%3==0 && i%5==0)
+    {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if(i%3==0){
+        console.log(`${i} -> Frontend`);
+    }
+    else if(i%5==0){
+        console.log(`${i} -> Simplified`);
+    }
+    else{
+    console.log(`${i} -> ${i}`);
+    }
 }
